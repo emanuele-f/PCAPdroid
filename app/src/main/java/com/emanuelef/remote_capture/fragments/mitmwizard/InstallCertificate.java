61330c4fb45da27904bde8db8f12c2329c800b34
emanuele-f <black.silver@hotmail.it>
2022-02-16 15:10:13 +0100

    Ability to install the CA certificate via Intent
    This works before Android 11. If the installation fails, fall back to
    export

--- app/src/main/java/com/emanuelef/remote_capture/fragments/mitmwizard/InstallCertificate.java
+++ app/src/main/java/com/emanuelef/remote_capture/fragments/mitmwizard/InstallCertificate.java
@@ -24,7 +24,9 @@ import android.content.ActivityNotFoundException;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.security.KeyChain;
 import android.util.Log;
 import android.view.View;
 
@@ -41,6 +43,7 @@ import com.emanuelef.remote_capture.MitmAddon;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
 import java.security.cert.X509Certificate;
 
 public class InstallCertificate extends StepFragment implements MitmListener {
@@ -48,15 +51,18 @@ public class InstallCertificate extends StepFragment implements MitmListener {
     private MitmAddon mAddon;
     private String mCaPem;
     private X509Certificate mCaCert;
+    private boolean mFallbackExport;
 
-    private final ActivityResultLauncher<Intent> certFileLauncher =
-            registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), this::certFileResult);
+    private final ActivityResultLauncher<Intent> certExportLauncher =
+            registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), this::certExportResult);
+    private final ActivityResultLauncher<Intent> certInstallLauncher =
+            registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), this::certInstallResult);
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         mStepLabel.setText(R.string.checking_the_certificate);
-        mStepButton.setText(R.string.export_action);
+        mStepButton.setText(canInstallCertViaIntent() ? R.string.install_action : R.string.export_action);
         mStepButton.setEnabled(false);
 
         mAddon = new MitmAddon(requireContext(), this);
@@ -90,7 +96,19 @@ public class InstallCertificate extends StepFragment implements MitmListener {
         MitmAddon.setDecryptionSetupDone(requireContext(), false);
     }
 
-    private void installCaCertificate() {
+    private boolean canInstallCertViaIntent() {
+        // On Android < 11, an intent can be used for cert installation
+        // On Android 11+, users must manually install the certificate from the settings
+        return((Build.VERSION.SDK_INT < Build.VERSION_CODES.R) && !mFallbackExport);
+    }
+
+    private void fallbackToCertExport() {
+        // If there are problems with the cert installation via Intent, fallback to export+install
+        mFallbackExport = true;
+        onMitmGetCaCertificateResult(mCaPem);
+    }
+
+    private void exportCaCertificate() {
         String fname = "PCAPdroid_CA.crt";
         Intent intent = new Intent(Intent.ACTION_CREATE_DOCUMENT);
         intent.addCategory(Intent.CATEGORY_OPENABLE);
@@ -98,26 +116,33 @@ public class InstallCertificate extends StepFragment implements MitmListener {
         intent.putExtra(Intent.EXTRA_TITLE, fname);
 
         if(!Utils.supportsFileDialog(requireContext(), intent)) {
+            Utils.showToastLong(requireContext(), R.string.no_activity_file_selection);
             certFail();
             return;
         }
 
-        // Certificate not installed
-        mStepIcon.setColorFilter(mWarnColor);
-        mStepLabel.setText(R.string.install_ca_certificate);
-
-        // TODO auto-install on older android versions
-        mStepButton.setEnabled(true);
-        mStepButton.setOnClickListener(v -> {
-            try {
-                certFileLauncher.launch(intent);
-            } catch (ActivityNotFoundException e) {
-                certFail();
-            }
-        });
+        try {
+            certExportLauncher.launch(intent);
+        } catch (ActivityNotFoundException e) {
+            Utils.showToastLong(requireContext(), R.string.no_activity_file_selection);
+            certFail();
+        }
     }
 
-    private void certFileResult(final ActivityResult result) {
+    private void installCaCertificate() {
+        Intent intent = KeyChain.createInstallIntent();
+        intent.putExtra(KeyChain.EXTRA_NAME, "PCAPdroid CA");
+        intent.putExtra(KeyChain.EXTRA_CERTIFICATE, mCaPem.getBytes(StandardCharsets.UTF_8));
+
+        try {
+            certInstallLauncher.launch(intent);
+        } catch (ActivityNotFoundException e) {
+            Utils.showToastLong(requireContext(), R.string.no_intent_handler_found);
+            fallbackToCertExport();
+        }
+    }
+
+    private void certExportResult(final ActivityResult result) {
         if((result.getResultCode() == Activity.RESULT_OK) && (result.getData() != null)) {
             Context ctx = requireContext();
             Uri cert_uri = result.getData().getData();
@@ -135,10 +160,20 @@ public class InstallCertificate extends StepFragment implements MitmListener {
         }
     }
 
+    private void certInstallResult(final ActivityResult result) {
+        if((result.getResultCode() == Activity.RESULT_OK) && Utils.isCAInstalled(mCaCert))
+            certOk();
+        else
+            fallbackToCertExport();
+    }
+
     @Override
     public void onMitmGetCaCertificateResult(@Nullable String ca_pem) {
         mAddon.disconnect();
         mCaPem = ca_pem;
+
+        // NOTE: onMitmGetCaCertificateResult can be called by fallbackToCertExport
+        mStepButton.setText(canInstallCertViaIntent() ? R.string.install_action : R.string.export_action);
 
         if(mCaPem != null) {
             Log.d(TAG, "Got certificate");
@@ -149,8 +184,22 @@ public class InstallCertificate extends StepFragment implements MitmListener {
                 if(Utils.isCAInstalled(mCaCert))
                     certOk();
                 else {
+                    // Cert not installed
                     MitmAddon.setDecryptionSetupDone(requireContext(), false);
-                    installCaCertificate();
+                    mStepIcon.setColorFilter(mWarnColor);
+                    mStepButton.setEnabled(true);
+
+                    if(canInstallCertViaIntent())
+                        mStepLabel.setText(R.string.install_ca_certificate);
+                    else
+                        mStepLabel.setText(R.string.export_ca_certificate);
+
+                    mStepButton.setOnClickListener((v) -> {
+                        if(canInstallCertViaIntent())
+                            installCaCertificate();
+                        else
+                            exportCaCertificate();
+                    });
                 }
             } else
                 certFail();
