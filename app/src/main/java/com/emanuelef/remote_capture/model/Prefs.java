0065c9a7d09333adfba1bbca9a1027a09caaecb5
emanuele-f <black.silver@hotmail.it>
2022-05-27 16:10:08 +0200

    Fix QUIC block preference value with decryption off

--- app/src/main/java/com/emanuelef/remote_capture/model/Prefs.java
+++ app/src/main/java/com/emanuelef/remote_capture/model/Prefs.java
@@ -124,5 +124,5 @@ public class Prefs {
     public static String getPCAPUri(SharedPreferences p)          { return(p.getString(PREF_PCAP_URI, "")); }
     public static boolean isTLSDecryptionSetupDone(SharedPreferences p) { return(p.getBoolean(PREF_TLS_DECRYPTION_SETUP_DONE, false)); }
     public static boolean getFullPayloadMode(SharedPreferences p) { return(p.getBoolean(PREF_FULL_PAYLOAD, false)); }
-    public static boolean blockQuic(SharedPreferences p)          { return(p.getBoolean(PREF_BLOCK_QUIC, false)); }
+    public static boolean blockQuic(SharedPreferences p)          { return(getTlsDecryptionEnabled(p) && p.getBoolean(PREF_BLOCK_QUIC, false)); }
 }
