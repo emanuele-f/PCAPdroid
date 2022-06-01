emanuele-f/PCAPdroid

commit bd6034b8c66c7e3c735358472ffffef02969a87e

Fix notification sometimes not removed on stop
Fixes #198

app/src/main/java/com/emanuelef/remote_capture/CaptureService.java
@@ -681,13 +681,22 @@ public class CaptureService extends VpnService implements Runnable {
      * when mCaptureThread terminates. */
     public static void stopService() {
         CaptureService captureService = INSTANCE;
+        Log.d(TAG, "stopService called (instance? " + (captureService != null) + ")");
+
         if(captureService == null)
             return;
 
         stopPacketLoop();
         captureService.signalServicesTermination();
 
-        captureService.stopForeground(true /* remove notification */);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N)
+            captureService.stopForeground(STOP_FOREGROUND_REMOVE);
+        else
+            captureService.stopForeground(true);
+
+        // this fixes notification not removed (reproduced on the Android 12 emulator)
+        NotificationManagerCompat.from(captureService).deleteNotificationChannel(NOTIFY_CHAN_VPNSERVICE);
+
         captureService.stopSelf();
     }
 

app/src/main/java/com/emanuelef/remote_capture/activities/MainActivity.java
@@ -732,6 +732,7 @@ public class MainActivity extends BaseActivity implements NavigationView.OnNavig
         intent.setType("*/*");
         intent.putExtra(Intent.EXTRA_TITLE, "sslkeylogfile.txt");
 
+        Log.d(TAG, "startExportSslkeylogfile: launching dialog");
         Utils.launchFileDialog(this, intent, sslkeyfileExportLauncher);
     }
 

commit 33b68936ea257bfb6305ba08cd17b3ac032a1ae2

Fix developer warning notification on Android 32
Bug introduced in bd6034b. Now notification is correctly removed

app/src/main/java/com/emanuelef/remote_capture/CaptureService.java
@@ -130,6 +130,7 @@ public class CaptureService extends VpnService implements Runnable {
     private boolean mDnsEncrypted;
     private boolean mStrictDnsNoticeShown;
     private boolean mQueueFull;
+    private boolean mStopping;
     private Blacklists mBlacklists;
     private MatchList mBlocklist;
     private MatchList mWhitelist;
@@ -196,6 +197,8 @@ public class CaptureService extends VpnService implements Runnable {
 
     @Override
     public int onStartCommand(@Nullable Intent intent, int flags, int startId) {
+        mStopping = false;
+
         // startForeground must always be called since the Service is being started with
         // ContextCompat.startForegroundService.
         // NOTE: since Android 12, startForeground cannot be called when the app is in background
@@ -516,6 +519,9 @@ public class CaptureService extends VpnService implements Runnable {
     }
 
     private void updateNotification() {
+        if(mStopping)
+            return;
+
         Notification notification = getStatusNotification();
         NotificationManagerCompat.from(this).notify(NOTIFY_ID_VPNSERVICE, notification);
     }
@@ -693,6 +699,8 @@ public class CaptureService extends VpnService implements Runnable {
         if(captureService == null)
             return;
 
+        captureService.mStopping = true;
+
         stopPacketLoop();
         captureService.signalServicesTermination();
 
@@ -700,9 +708,6 @@ public class CaptureService extends VpnService implements Runnable {
             captureService.stopForeground(STOP_FOREGROUND_REMOVE);
         else
             captureService.stopForeground(true);
-
-        // this fixes notification not removed (reproduced on the Android 12 emulator)
-        NotificationManagerCompat.from(captureService).deleteNotificationChannel(NOTIFY_CHAN_VPNSERVICE);
 
         captureService.stopSelf();
     }