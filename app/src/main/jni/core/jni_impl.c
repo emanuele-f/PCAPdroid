emanuele-f/PCAPdroid

commit 275eefaa1c45c24dfc9de8269e8df3bfdf1ecf43

Rework PCAP dump
Encapsulate logic into a new dumper API, preparing for pcapng
support

app/src/main/jni/core/jni_impl.c
@@ -21,7 +21,6 @@
 
 #include <pthread.h>
 #include "pcapdroid.h"
-#include "pcap_utils.h"
 #include "common/utils.h"
 #include "log_writer.h"
 #include "port_map.h"
@@ -92,7 +91,8 @@ static void sendStatsDump(pcapdroid_t *pd) {
 
     (*env)->CallVoidMethod(env, stats_obj, mids.statsSetData,
                            allocs_summary,
-                           capstats->sent_bytes, capstats->rcvd_bytes, pd->pcap_dump.tot_size,
+                           capstats->sent_bytes, capstats->rcvd_bytes,
+                           pd->pcap_dump.dumper ? pcap_get_dump_size(pd->pcap_dump.dumper) : 0,
                            capstats->sent_pkts, capstats->rcvd_pkts,
                            min(pd->num_dropped_pkts, INT_MAX), pd->num_dropped_connections,
                            stats->num_open_sockets, stats->all_max_fd, active_conns, tot_conns,
@@ -109,16 +109,16 @@ static void sendStatsDump(pcapdroid_t *pd) {
 
 /* ******************************************************* */
 
-static void sendPcapDump(pcapdroid_t *pd) {
+static void sendPcapDump(struct pcapdroid *pd, int8_t *buf, int dump_size) {
     JNIEnv *env = pd->env;
 
     //log_d("Exporting a %d B PCAP buffer", pd->pcap_dump.buffer_idx);
 
-    jbyteArray barray = (*env)->NewByteArray(env, pd->pcap_dump.buffer_idx);
+    jbyteArray barray = (*env)->NewByteArray(env, dump_size);
     if(jniCheckException(env))
         return;
 
-    (*env)->SetByteArrayRegion(env, barray, 0, pd->pcap_dump.buffer_idx, pd->pcap_dump.buffer);
+    (*env)->SetByteArrayRegion(env, barray, 0, dump_size, buf);
     (*env)->CallVoidMethod(env, pd->capture_service, mids.dumpPcapData, barray);
     jniCheckException(env);
 
@@ -590,6 +590,7 @@ Java_com_emanuelef_remote_1capture_CaptureService_runPacketLoop(JNIEnv *env, jcl
             .payload_mode = (payload_mode_t) getIntPref(env, vpn, "getPayloadMode"),
             .pcap_dump = {
                     .enabled = (bool) getIntPref(env, vpn, "pcapDumpEnabled"),
+                    .trailer_enabled = (bool)getIntPref(env, vpn, "addPcapdroidTrailer"),
                     .snaplen = getIntPref(env, vpn, "getSnaplen"),
                     .max_pkts_per_flow = getIntPref(env, vpn, "getMaxPktsPerFlow"),
                     .max_dump_size = getIntPref(env, vpn, "getMaxDumpSize"),
@@ -612,9 +613,6 @@ Java_com_emanuelef_remote_1capture_CaptureService_runPacketLoop(JNIEnv *env, jcl
 
     if(pd.socks5.enabled)
         getSocks5ProxyAuth(&pd);
-
-    // Enable or disable the PCAPdroid trailer
-    pcap_set_pcapdroid_trailer((bool)getIntPref(env, vpn, "addPcapdroidTrailer"));
 
     if(!pd.root_capture)
         pd.vpn.tunfd = tunfd;
@@ -698,16 +696,25 @@ Java_com_emanuelef_remote_1capture_CaptureService_setDnsServer(JNIEnv *env, jcla
 
 JNIEXPORT jbyteArray JNICALL
 Java_com_emanuelef_remote_1capture_CaptureService_getPcapHeader(JNIEnv *env, jclass clazz) {
-    struct pcap_hdr_s pcap_hdr;
+    pcapdroid_t *pd = global_pd;
+    if(!pd || !pd->pcap_dump.dumper) {
+        log_e("NULL pd/dumper instance");
+        return false;
+    }
 
-    int snaplen = global_pd ? global_pd->pcap_dump.snaplen : 65535;
-    pcap_build_hdr(snaplen, &pcap_hdr);
-
-    jbyteArray barray = (*env)->NewByteArray(env, sizeof(struct pcap_hdr_s));
-    if((barray == NULL) || jniCheckException(env))
+    char *pcap_hdr = NULL;
+    int hdr_size = pcap_get_header(pd->pcap_dump.dumper, &pcap_hdr);
+    if((hdr_size < 0) || !pcap_hdr)
         return NULL;
 
-    (*env)->SetByteArrayRegion(env, barray, 0, sizeof(struct pcap_hdr_s), (jbyte*)&pcap_hdr);
+    jbyteArray barray = (*env)->NewByteArray(env, hdr_size);
+    if((barray == NULL) || jniCheckException(env)) {
+        free(pcap_hdr);
+        return NULL;
+    }
+
+    (*env)->SetByteArrayRegion(env, barray, 0, hdr_size, (jbyte*)pcap_hdr);
+    pd_free(pcap_hdr);
 
     if(jniCheckException(env)) {
         (*env)->DeleteLocalRef(env, barray);

app/src/main/jni/core/pcap_dump.c
@@ -1,0 +1,228 @@
+/*
+ * This file is part of PCAPdroid.
+ *
+ * PCAPdroid is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * PCAPdroid is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with PCAPdroid.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Copyright 2023 - Emanuele Faranda
+ */
+
+#include <linux/if_ether.h>
+#include "common/utils.h"
+#include "pcapdroid.h"
+#include "pcap_dump.h"
+
+#define LINKTYPE_ETHERNET 1
+#define LINKTYPE_RAW      101
+
+#define PCAPDROID_TRAILER_MAGIC 0x01072021
+#define MAX_PCAP_DUMP_DELAY_MS 1000
+#define PCAP_BUFFER_SIZE (512*1024) // 512K
+
+struct pcap_dumper {
+    pcap_dump_mode_t mode;
+    pcap_dump_callback *dump_cb;
+    pcapdroid_t *pd;
+    int snaplen;
+    uint64_t max_dump_size;
+    uint64_t dump_size;
+    uint64_t last_dump_ms;
+
+    // the crc32 implementation requires 4-bytes aligned accesses.
+    // frames are padded to honor the 4-bytes alignment.
+    int8_t *buffer  __attribute__((aligned (4)));
+    int bufsize;
+    int buffer_idx;
+};
+
+/* ******************************************************* */
+
+pcap_dumper_t* pcap_new_dumper(pcap_dump_mode_t mode, int snaplen, uint64_t max_dump_size,
+                               pcap_dump_callback dumpcb, pcapdroid_t *pd) {
+    pcap_dumper_t *dumper = pd_calloc(1, sizeof(pcap_dumper_t));
+    if(!dumper) {
+        log_e("calloc(pcap_dumper_t) failed with code %d/%s",
+              errno, strerror(errno));
+        return NULL;
+    }
+
+    dumper->buffer = pd_malloc(PCAP_BUFFER_SIZE);
+    if(!dumper->buffer) {
+        log_e("malloc(pcap_dumper_t buffer) failed with code %d/%s",
+              errno, strerror(errno));
+        pd_free(dumper);
+        return NULL;
+    }
+
+    dumper->snaplen = snaplen;
+    dumper->mode = mode;
+    dumper->max_dump_size = max_dump_size;
+    dumper->dump_cb = dumpcb;
+    dumper->pd = pd;
+
+    return dumper;
+}
+
+/* ******************************************************* */
+
+static void export_buffer(pcap_dumper_t *dumper) {
+    if(dumper->buffer_idx == 0)
+        return;
+
+    if(dumper->dump_cb)
+        dumper->dump_cb(dumper->pd, dumper->buffer, dumper->buffer_idx);
+
+    dumper->buffer_idx = 0;
+    dumper->last_dump_ms = dumper->pd->now_ms;
+}
+
+/* ******************************************************* */
+
+void pcap_destroy_dumper(pcap_dumper_t *dumper) {
+    export_buffer(dumper);
+
+    pd_free(dumper->buffer);
+    pd_free(dumper);
+}
+
+/* ******************************************************* */
+
+/* Get a buffer (out) representing a PCAP header.
+ * Returns the buffer size on success, -1 on error. The out buffer must be free by the called with pd_free. */
+int pcap_get_header(pcap_dumper_t *dumper, char **out) {
+    struct pcap_hdr *pcap_hdr = pd_malloc(sizeof(struct pcap_hdr));
+    if(!pcap_hdr)
+        return -1;
+
+    pcap_hdr->magic_number = 0xa1b2c3d4;
+    pcap_hdr->version_major = 2;
+    pcap_hdr->version_minor = 4;
+    pcap_hdr->thiszone = 0;
+    pcap_hdr->sigfigs = 0;
+    pcap_hdr->snaplen = dumper->snaplen;
+    pcap_hdr->network = (dumper->mode == PCAP_DUMP_WITH_TRAILER) ? LINKTYPE_ETHERNET : LINKTYPE_RAW;
+
+    *out = (char*)pcap_hdr;
+    return sizeof(struct pcap_hdr);
+}
+
+/* ******************************************************* */
+
+/* Returns the size of a PCAP record */
+int pcap_rec_size(pcap_dumper_t *dumper, int pkt_len) {
+    if(dumper->mode == PCAP_DUMP_WITH_TRAILER) {
+        pkt_len += (int)(sizeof(pcapdroid_trailer_t) + sizeof(struct ethhdr));
+
+        // Pad the frame so that the buffer keeps its 4-bytes alignment
+        pkt_len += (~pkt_len + 1) & 0x3;
+    }
+
+    return(min(pkt_len, dumper->snaplen) + (int)sizeof(struct pcap_rec));
+}
+
+/* ******************************************************* */
+
+bool pcap_check_export(pcap_dumper_t *dumper) {
+    if((dumper->buffer_idx > 0) && (dumper->pd->now_ms - dumper->last_dump_ms) >= MAX_PCAP_DUMP_DELAY_MS) {
+        export_buffer(dumper);
+        return true;
+    }
+    return false;
+}
+
+/* ******************************************************* */
+
+int pcap_get_dump_size(pcap_dumper_t *dumper) {
+    return dumper->dump_size;
+}
+
+/* ******************************************************* */
+
+/* Dump a single packet into the buffer. Returns false if PCAP dump must be stopped (e.g. if max
+ * dump size reached or an error occurred). */
+bool pcap_dump_packet(pcap_dumper_t *dumper, const char *pkt, int pktlen,
+                      const struct timeval *tv, int uid) {
+    int tot_rec_size = pcap_rec_size(dumper, pktlen);
+
+    if((PCAP_BUFFER_SIZE - dumper->buffer_idx) <= tot_rec_size)
+        export_buffer(dumper);
+
+    if ((PCAP_BUFFER_SIZE - dumper->buffer_idx) <= tot_rec_size) {
+        log_e("Invalid buffer size [size=%d, idx=%d, dump_size=%d]",
+              PCAP_BUFFER_SIZE, dumper->buffer_idx, tot_rec_size);
+        return false;
+    } else if((dumper->max_dump_size > 0) &&
+            ((dumper->dump_size + tot_rec_size) >= dumper->max_dump_size)) {
+        log_i("Max dump size reached, stop the dump");
+        return false;
+    }
+
+    // NOTE: buffer_idx may be reset by export_buffer above
+    int8_t *buffer = dumper->buffer + dumper->buffer_idx;
+    pcap_rec_t *pcap_rec = (pcap_rec_t*) buffer;
+    int offset = 0;
+
+    pcap_rec->ts_sec = tv->tv_sec;
+    pcap_rec->ts_usec = tv->tv_usec;
+    pcap_rec->incl_len = tot_rec_size - (int)sizeof(struct pcap_rec);
+    pcap_rec->orig_len = pktlen;
+    buffer += sizeof(struct pcap_rec);
+
+    if(dumper->mode == PCAP_DUMP_WITH_TRAILER) {
+        if((((uint64_t)buffer) & 0x03) != 0)
+            log_w("Unaligned buffer!");
+
+        // Insert the bogus header: both the MAC addresses are 0
+        struct ethhdr *eth = (struct ethhdr*) buffer;
+        memset(eth, 0, sizeof(struct ethhdr));
+        eth->h_proto = htons((((*pkt) >> 4) == 4) ? ETH_P_IP : ETH_P_IPV6);
+
+        pcap_rec->orig_len += sizeof(struct ethhdr);
+        offset += sizeof(struct ethhdr);
+    }
+
+    int payload_to_copy = min(pktlen, pcap_rec->incl_len - offset);
+    memcpy(buffer + offset, pkt, payload_to_copy);
+    offset += payload_to_copy;
+
+    if((dumper->mode == PCAP_DUMP_WITH_TRAILER) &&
+       ((pcap_rec->incl_len - offset) >= sizeof(pcapdroid_trailer_t))) {
+        // Pad the frame so that the buffer keeps its 4-bytes alignment
+        // The padding is inserted before the PCAPdroid trailer so that accesses to pcapdroid_trailer_t
+        // are also aligned.
+        uint8_t padding = (~offset + 1) & 0x03;
+
+        for(uint8_t i=0; i<padding; i++)
+            buffer[offset++] = 0x00;
+
+        // Populate the trailer
+        pcapdroid_trailer_t *trailer = (pcapdroid_trailer_t*)(buffer + offset);
+        memset(trailer, 0, sizeof(*trailer));
+
+        trailer->magic = htonl(PCAPDROID_TRAILER_MAGIC);
+        trailer->uid = htonl(uid);
+        get_appname_by_uid(dumper->pd, uid, trailer->appname, sizeof(trailer->appname));
+
+        //clock_t start = clock();
+        trailer->fcs = crc32((u_char*) buffer, pcap_rec->incl_len - 4, 0);
+        //double cpu_time_used = ((double) (clock() - start)) / CLOCKS_PER_SEC;
+        //log_d("crc cpu_time_used: %f sec", cpu_time_used);
+
+        pcap_rec->orig_len += padding + sizeof(*trailer);
+    }
+
+    dumper->buffer_idx += tot_rec_size;
+    dumper->dump_size += tot_rec_size;
+    pcap_check_export(dumper);
+    return true;
+}

app/src/main/jni/core/pcap_dump.h
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU General Public License
  * along with PCAPdroid.  If not, see <http://www.gnu.org/licenses/>.
  *
- * Copyright 2020-21 - Emanuele Faranda
+ * Copyright 2023 - Emanuele Faranda
  */
 
 #ifndef __MY_PCAP_H__
@@ -22,9 +22,17 @@
 
 #include <stdlib.h>
 #include <stdint.h>
-#include "common/utils.h"
 
-typedef struct pcap_hdr_s {
+/* Packet dump module, dumping packet records in the PCAP/PCAPNG format.
+ * Packets are first buffered and then exported periodically to the callback. pcap_check_export must
+ * be called periodically to ensure that buffered packets are exported on time.
+ *
+ * The PCAP/PCAPNG headers are *not* dumped, use pcap_get_header to get the header to be dumped. This
+ * allows, for example, multiple HTTP clients to connect at different times, each one getting a valid
+ * PCAP header. */
+typedef struct pcap_dumper pcap_dumper_t;
+
+typedef struct pcap_hdr {
     uint32_t magic_number;
     uint16_t version_major;
     uint16_t version_minor;
@@ -32,16 +40,20 @@ typedef struct pcap_hdr_s {
     uint32_t sigfigs;
     uint32_t snaplen;
     uint32_t network;
-} __packed pcap_hdr_s;
+} __attribute__((packed)) pcap_hdr_t;
 
-typedef struct pcaprec_hdr_s {
+typedef struct pcap_rec {
     uint32_t ts_sec;
     uint32_t ts_usec;
     uint32_t incl_len;
     uint32_t orig_len;
-} __packed pcaprec_hdr_s;
+} __attribute__((packed)) pcap_rec_t;
 
-#define PCAPDROID_TRAILER_MAGIC 0x01072021
+typedef enum {
+    PCAP_DUMP,                // PCAP file
+    PCAP_DUMP_WITH_TRAILER,   // PCAP file with PCAPdroid trailer
+    PCAPNG_DUMP,              // PcapNg file
+} pcap_dump_mode_t;
 
 /* A trailer to the packet which contains PCAPdroid-specific information.
  * When pcapdroid_trailer is set, the raw packet will be prepended with a bogus ethernet header,
@@ -56,12 +68,17 @@ typedef struct pcapdroid_trailer {
     int32_t uid;
     char appname[20];
     uint32_t fcs;
-} __packed pcapdroid_trailer_t;
+} __attribute__((packed)) pcapdroid_trailer_t;
 
-void pcap_set_pcapdroid_trailer(uint8_t enabled);
-void pcap_build_hdr(int snaplen, struct pcap_hdr_s *pcap_hdr);
-int pcap_rec_size(int snaplen, int pkt_len);
-void pcap_dump_rec(pcapdroid_t *pd, u_char *buffer, const char *pkt, int pktlen,
-                   const struct timeval *tv, int uid);
+struct pcapdroid;
+typedef void pcap_dump_callback(struct pcapdroid *pd, const int8_t *buf, int dump_size);
+
+pcap_dumper_t* pcap_new_dumper(pcap_dump_mode_t mode, int snaplen, uint64_t max_dump_size,
+                               pcap_dump_callback dumpcb, struct pcapdroid *pd);
+void pcap_destroy_dumper(pcap_dumper_t *dumper);
+bool pcap_dump_packet(pcap_dumper_t *dumper, const char *pkt, int pktlen, const struct timeval *tv, int uid);
+int pcap_get_header(pcap_dumper_t *dumper, char **out);
+int pcap_get_dump_size(pcap_dumper_t *dumper);
+bool pcap_check_export(pcap_dumper_t *dumper);
 
 #endif // __MY_PCAP_H__

app/src/main/java/com/emanuelef/remote_capture/Utils.java
@@ -543,7 +543,7 @@ public class Utils {
         return new Iterator<Integer>() {
             @Override
             public boolean hasNext() {
-                // 16: sizeof(pcaprec_hdr_s)
+                // 16: sizeof(pcap_rec)
                 return(buf.remaining() > 16);
             }
 

app/src/main/jni/core/CMakeLists.txt
@@ -8,7 +8,7 @@ add_library(capture SHARED
         ndpi_config.c
         crc32.c
         blacklist.c
-        pcap_utils.c
+        pcap_dump.c
         log_writer.c
         port_map.c
         jni_impl.c)

app/src/main/jni/core/pcap_utils.c
@@ -1,119 +1,0 @@
-/*
- * This file is part of PCAPdroid.
- *
- * PCAPdroid is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * PCAPdroid is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with PCAPdroid.  If not, see <http://www.gnu.org/licenses/>.
- *
- * Copyright 2020-21 - Emanuele Faranda
- */
-
-#include <linux/if_ether.h>
-#include "common/utils.h"
-#include "pcapdroid.h"
-#include "pcap_utils.h"
-
-#define LINKTYPE_ETHERNET 1
-#define LINKTYPE_RAW      101
-
-static uint8_t pcapdroid_trailer = 0;
-
-/* ******************************************************* */
-
-/* Enable the addition of the pcapdroid_trailer_t to the PCAP */
-void pcap_set_pcapdroid_trailer(uint8_t enabled) {
-    pcapdroid_trailer = enabled;
-}
-
-/* ******************************************************* */
-
-void pcap_build_hdr(int snaplen, struct pcap_hdr_s *pcap_hdr) {
-    pcap_hdr->magic_number = 0xa1b2c3d4;
-    pcap_hdr->version_major = 2;
-    pcap_hdr->version_minor = 4;
-    pcap_hdr->thiszone = 0;
-    pcap_hdr->sigfigs = 0;
-    pcap_hdr->snaplen = snaplen;
-    pcap_hdr->network = pcapdroid_trailer ? LINKTYPE_ETHERNET : LINKTYPE_RAW;
-}
-
-/* ******************************************************* */
-
-/* Returns the size of a PCAP record */
-int pcap_rec_size(int snaplen, int pkt_len) {
-    if(pcapdroid_trailer) {
-        pkt_len += (int)(sizeof(pcapdroid_trailer_t) + sizeof(struct ethhdr));
-
-        // Pad the frame so that the buffer keeps its 4-bytes alignment
-        pkt_len += (~pkt_len + 1) & 0x3;
-    }
-
-    return(min(pkt_len, snaplen) + (int)sizeof(struct pcaprec_hdr_s));
-}
-
-/* ******************************************************* */
-
-/* Dumps a packet into the provided buffer. The buffer must have at least pcap_rec_size()
- * bytes available */
-void pcap_dump_rec(pcapdroid_t *pd, u_char *buffer, const char *pkt, int pktlen,
-                   const struct timeval *tv, int uid) {
-    //const zdtun_pkt_t *pkt = pctx->pkt;
-    struct pcaprec_hdr_s *pcap_rec = (pcaprec_hdr_s*) buffer;
-    int offset = 0;
-    int snaplen = pd->pcap_dump.snaplen;
-
-    pcap_rec->ts_sec = tv->tv_sec;
-    pcap_rec->ts_usec = tv->tv_usec;
-    pcap_rec->incl_len = pcap_rec_size(snaplen, pktlen) - (int)sizeof(struct pcaprec_hdr_s);
-    pcap_rec->orig_len = pktlen;
-    buffer += sizeof(struct pcaprec_hdr_s);
-
-    if(pcapdroid_trailer) {
-        if((((uint64_t)buffer) & 0x03) != 0)
-            log_w("Unaligned buffer!");
-
-        // Insert the bogus header: both the MAC addresses are 0
-        struct ethhdr *eth = (struct ethhdr*) buffer;
-        memset(eth, 0, sizeof(struct ethhdr));
-        eth->h_proto = htons((((*pkt) >> 4) == 4) ? ETH_P_IP : ETH_P_IPV6);
-
-        pcap_rec->orig_len += sizeof(struct ethhdr);
-        offset += sizeof(struct ethhdr);
-    }
-
-    int payload_to_copy = min(pktlen, pcap_rec->incl_len - offset);
-    memcpy(buffer + offset, pkt, payload_to_copy);
-    offset += payload_to_copy;
-
-    if(pcapdroid_trailer &&
-       ((pcap_rec->incl_len - offset) >= sizeof(pcapdroid_trailer_t))) {
-        // Pad the frame so that the buffer keeps its 4-bytes alignment
-        // The padding is inserted before the PCAPdroid trailer so that accesses to pcapdroid_trailer_t
-        // are also aligned.
-        uint8_t padding = (~offset + 1) & 0x03;
-
-        for(uint8_t i=0; i<padding; i++)
-            buffer[offset++] = 0x00;
-
-        // Populate the custom data
-        pcapdroid_trailer_t *cdata = (pcapdroid_trailer_t*)(buffer + offset);
-
-        fill_custom_data(cdata, pd, uid);
-
-        //clock_t start = clock();
-        cdata->fcs = crc32(buffer, pcap_rec->incl_len - 4, 0);
-        //double cpu_time_used = ((double) (clock() - start)) / CLOCKS_PER_SEC;
-        //log_d("crc cpu_time_used: %f sec", cpu_time_used);
-
-        pcap_rec->orig_len += padding + sizeof(pcapdroid_trailer_t);
-    }
-}

app/src/main/jni/core/pcapdroid.c
@@ -21,7 +21,7 @@
 #include <assert.h> // NOTE: look for "assertion" in logcat
 #include <pthread.h>
 #include "pcapdroid.h"
-#include "pcap_utils.h"
+#include "pcap_dump.h"
 #include "common/utils.h"
 #include "pcapd/pcapd.h"
 #include "ndpi_protocol_ids.h"
@@ -953,23 +953,9 @@ static int check_blacklisted_conn_cb(pcapdroid_t *pd, const zdtun_5tuple_t *tupl
 
 /* ******************************************************* */
 
-static void sendPcapDump(pcapdroid_t *pd) {
-    if(pd->pcap_dump.buffer_idx == 0)
-        return;
-
-    if(pd->cb.send_pcap_dump)
-        pd->cb.send_pcap_dump(pd);
-
-    pd->pcap_dump.buffer_idx = 0;
-    pd->pcap_dump.last_dump_ms = pd->now_ms;
-}
-
-/* ******************************************************* */
-
-static void stop_pcap_dump(pcapdroid_t *pd){
-    sendPcapDump(pd);
-    pd_free(pd->pcap_dump.buffer);
-    pd->pcap_dump.buffer = NULL;
+static void stop_pcap_dump(pcapdroid_t *pd) {
+    pcap_destroy_dumper(pd->pcap_dump.dumper);
+    pd->pcap_dump.dumper = NULL;
 
     if(pd->cb.stop_pcap_dump)
         pd->cb.stop_pcap_dump(pd);
@@ -998,8 +984,8 @@ void pd_housekeeping(pcapdroid_t *pd) {
               pd->now_ms - last_connections_dump,
               pd->new_conns.cur_items, pd->conns_updates.cur_items);*/
 
-        if((pd->new_conns.cur_items != 0) || (pd->conns_updates.cur_items != 0)) {
-            if(pd->cb.send_connections_dump)
+        if ((pd->new_conns.cur_items != 0) || (pd->conns_updates.cur_items != 0)) {
+            if (pd->cb.send_connections_dump)
                 pd->cb.send_connections_dump(pd);
             conns_clear(pd, &pd->new_conns, false);
             conns_clear(pd, &pd->conns_updates, false);
@@ -1008,10 +994,9 @@ void pd_housekeeping(pcapdroid_t *pd) {
         last_connections_dump = pd->now_ms;
         next_connections_dump = pd->now_ms + CONNECTION_DUMP_UPDATE_FREQUENCY_MS;
         netd_resolve_waiting = 0;
-    } else if ((pd->pcap_dump.buffer_idx > 0)
-               && (pd->now_ms - pd->pcap_dump.last_dump_ms) >= MAX_JAVA_DUMP_DELAY_MS) {
-        sendPcapDump(pd);
-    } else if(pd->malware_detection.enabled) {
+    } else if(pd->pcap_dump.dumper && pcap_check_export(pd->pcap_dump.dumper))
+        ;
+    else if(pd->malware_detection.enabled) {
         // Malware detection
         if(pd->malware_detection.reload_in_progress) {
             if(pd->malware_detection.reload_done) {
@@ -1082,16 +1067,6 @@ void pd_refresh_time(pcapdroid_t *pd) {
 
 /* ******************************************************* */
 
-void fill_custom_data(struct pcapdroid_trailer *cdata, pcapdroid_t *pd, int uid) {
-    memset(cdata, 0, sizeof(*cdata));
-
-    cdata->magic = htonl(PCAPDROID_TRAILER_MAGIC);
-    cdata->uid = htonl(uid);
-    get_appname_by_uid(pd, uid, cdata->appname, sizeof(cdata->appname));
-}
-
-/* ******************************************************* */
-
 /* Process the packet (e.g. perform DPI) and fill the packet context. */
 void pd_process_packet(pcapdroid_t *pd, zdtun_pkt_t *pkt, bool is_tx, const zdtun_5tuple_t *tuple,
                        pd_conn_t *data, struct timeval *tv, pkt_context_t *pctx) {
@@ -1119,29 +1094,11 @@ void pd_process_packet(pcapdroid_t *pd, zdtun_pkt_t *pkt, bool is_tx, const zdtu
 /* ******************************************************* */
 
 void pd_dump_packet(pcapdroid_t *pd, const char *pktbuf, int pktlen, const struct timeval *tv, int uid) {
-    if(!pd->pcap_dump.buffer)
+    if(!pd->pcap_dump.dumper)
         return;
 
-    int rec_size = pcap_rec_size(pd->pcap_dump.snaplen, pktlen);
-    if ((JAVA_PCAP_BUFFER_SIZE - pd->pcap_dump.buffer_idx) <= rec_size) {
-        // Flush the buffer
-        sendPcapDump(pd);
-    }
-
-    if ((JAVA_PCAP_BUFFER_SIZE - pd->pcap_dump.buffer_idx) <= rec_size)
-        log_e("Invalid buffer size [size=%d, idx=%d, tot_size=%d]",
-              JAVA_PCAP_BUFFER_SIZE, pd->pcap_dump.buffer_idx, rec_size);
-    else if((pd->pcap_dump.max_dump_size > 0) &&
-            ((pd->pcap_dump.tot_size + rec_size) >= pd->pcap_dump.max_dump_size)) {
-        log_i("Max dump size reached, stop the dump");
+    if(!pcap_dump_packet(pd->pcap_dump.dumper, pktbuf, pktlen, tv, uid))
         stop_pcap_dump(pd);
-    } else {
-        pcap_dump_rec(pd, (u_char *) pd->pcap_dump.buffer + pd->pcap_dump.buffer_idx,
-                      pktbuf, pktlen, tv, uid);
-
-        pd->pcap_dump.buffer_idx += rec_size;
-        pd->pcap_dump.tot_size += rec_size;
-    }
 }
 
 /* ******************************************************* */
@@ -1170,7 +1127,7 @@ void pd_account_stats(pcapdroid_t *pd, pkt_context_t *pctx) {
     data->update_type |= CONN_UPDATE_STATS;
     pd_notify_connection_update(pd, pctx->tuple, pctx->data);
 
-    if((pd->pcap_dump.buffer) &&
+    if((pd->pcap_dump.dumper) &&
             ((pd->pcap_dump.max_pkts_per_flow <= 0) ||
                 ((data->sent_pkts + data->rcvd_pkts) <= pd->pcap_dump.max_pkts_per_flow)))
         pd_dump_packet(pd, pkt->buf, pkt->len, &pctx->tv, pctx->data->uid);
@@ -1203,16 +1160,18 @@ int pd_run(pcapdroid_t *pd) {
     }
 
     if(pd->pcap_dump.enabled) {
-        pd->pcap_dump.buffer = pd_malloc(JAVA_PCAP_BUFFER_SIZE);
-        pd->pcap_dump.buffer_idx = 0;
         int max_snaplen = pd->root_capture ? PCAPD_SNAPLEN : VPN_BUFFER_SIZE;
 
+        // use the snaplen provided by the API
         if((pd->pcap_dump.snaplen <= 0) || (pd->pcap_dump.snaplen > max_snaplen))
             pd->pcap_dump.snaplen = max_snaplen;
 
-        if(!pd->pcap_dump.buffer) {
-            log_f("malloc(pcap_dump.buffer) failed with code %d/%s",
-                        errno, strerror(errno));
+        // TODO pcapng
+        pd->pcap_dump.dumper = pcap_new_dumper(pd->pcap_dump.trailer_enabled ? PCAP_DUMP_WITH_TRAILER : PCAP_DUMP,
+                                               pd->pcap_dump.snaplen, pd->pcap_dump.max_dump_size, pd->cb.send_pcap_dump,
+                                               pd);
+        if(!pd->pcap_dump.dumper) {
+            log_f("Could not initialize the PCAP dumper");
             running = false;
         }
     }
@@ -1277,7 +1236,7 @@ int pd_run(pcapdroid_t *pd) {
     ndpi_exit_detection_module(pd->ndpi);
 #endif
 
-    if(pd->pcap_dump.buffer)
+    if(pd->pcap_dump.dumper)
         stop_pcap_dump(pd);
 
     uid_to_app_t *e, *tmp;

commit 9628fd6ce9fa2cdee6105ad02f7a759416ca39a3

Fix invalid capture stats
Bug was introduced in 275eefa

app/src/main/jni/core/jni_impl.c
@@ -74,7 +74,7 @@ static void sendStatsDump(pcapdroid_t *pd) {
     const zdtun_statistics_t *stats = &pd->stats;
     jstring allocs_summary =
 #ifdef PCAPDROID_TRACK_ALLOCS
-            (*pd->env)->NewStringUTF(pd->env, get_allocs_summary());
+    (*pd->env)->NewStringUTF(pd->env, get_allocs_summary());
 #else
     NULL;
 #endif
@@ -92,7 +92,7 @@ static void sendStatsDump(pcapdroid_t *pd) {
     (*env)->CallVoidMethod(env, stats_obj, mids.statsSetData,
                            allocs_summary,
                            capstats->sent_bytes, capstats->rcvd_bytes,
-                           pd->pcap_dump.dumper ? pcap_get_dump_size(pd->pcap_dump.dumper) : 0,
+                           (jlong)(pd->pcap_dump.dumper ? pcap_get_dump_size(pd->pcap_dump.dumper) : 0),
                            capstats->sent_pkts, capstats->rcvd_pkts,
                            min(pd->num_dropped_pkts, INT_MAX), pd->num_dropped_connections,
                            stats->num_open_sockets, stats->all_max_fd, active_conns, tot_conns,

app/src/main/jni/core/pcap_dump.c
@@ -297,7 +297,7 @@ bool pcap_check_export(pcap_dumper_t *dumper) {
 
 /* ******************************************************* */
 
-int pcap_get_dump_size(pcap_dumper_t *dumper) {
+uint64_t pcap_get_dump_size(pcap_dumper_t *dumper) {
     return dumper->dump_size;
 }
 

app/src/main/jni/core/pcap_dump.h
@@ -128,7 +128,7 @@ void pcap_destroy_dumper(pcap_dumper_t *dumper);
 bool pcap_dump_packet(pcap_dumper_t *dumper, const char *pkt, int pktlen, const struct timeval *tv, int uid);
 bool pcap_dump_secret(pcap_dumper_t *dumper, int8_t *sec_data, int seclen);
 int pcap_get_preamble(pcap_dumper_t *dumper, char **out);
-int pcap_get_dump_size(pcap_dumper_t *dumper);
+uint64_t pcap_get_dump_size(pcap_dumper_t *dumper);
 bool pcap_check_export(pcap_dumper_t *dumper);
 
 #endif // __MY_PCAP_H__