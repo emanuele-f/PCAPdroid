emanuele-f/PCAPdroid

commit bf6e834f178984b77eeca414d84bec029f0902f0

Fix root permission request on read from pcap
INTERNET_ACROSS_USERS was requested when trying to read from a PCAP file,
which triggered a failure toast in most cases

app/src/main/java/com/emanuelef/remote_capture/CaptureService.java
@@ -1062,7 +1062,7 @@ public class CaptureService extends VpnService implements Runnable {
     // Inside the mCaptureThread
     @Override
     public void run() {
-        if(mSettings.root_capture || mSettings.readFromPcap()) {
+        if(mSettings.root_capture) {
             // Check for INTERACT_ACROSS_USERS, required to query apps of other users/work profiles
             if(checkCallingOrSelfPermission(Utils.INTERACT_ACROSS_USERS) != PackageManager.PERMISSION_GRANTED) {
                 boolean success = Utils.rootGrantPermission(this, Utils.INTERACT_ACROSS_USERS);

commit ebba4518f590900e949b24b865c9fa3f6f033bd1

Fix broken open PCAP file feature
Broken since bf6e834f (v1.6.9)

app/src/main/java/com/emanuelef/remote_capture/CaptureService.java
@@ -1093,9 +1093,9 @@ public class CaptureService extends VpnService implements Runnable {
     // Inside the mCaptureThread
     @Override
     public void run() {
-        if(mSettings.root_capture) {
+        if(mSettings.root_capture || mSettings.readFromPcap()) {
             // Check for INTERACT_ACROSS_USERS, required to query apps of other users/work profiles
-            if(checkCallingOrSelfPermission(Utils.INTERACT_ACROSS_USERS) != PackageManager.PERMISSION_GRANTED) {
+            if(mSettings.root_capture && (checkCallingOrSelfPermission(Utils.INTERACT_ACROSS_USERS) != PackageManager.PERMISSION_GRANTED)) {
                 boolean success = Utils.rootGrantPermission(this, Utils.INTERACT_ACROSS_USERS);
                 mHandler.post(() -> Utils.showToast(this, success ? R.string.permission_granted : R.string.permission_grant_fail, "INTERACT_ACROSS_USERS"));
             }